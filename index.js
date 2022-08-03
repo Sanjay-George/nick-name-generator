16cc6a5fff60a2edafc753bc76fd96fde74d0434
Sanjay-George <sanjaygeorge16@gmail.com>
2022-08-03 06:58:03 +0200

    Add nick name endpoint in service
    - Use facade pattern - acts like reverse proxy, but not apigateway
    
    TODO: Handle partial failure
    
    Signed-off-by: Sanjay-George <sanjaygeorge16@gmail.com>

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express, { response } from 'express';
 import os from 'os';
 import fetch from 'node-fetch';
 
+// TODO: read config based on environment variable - isDevEnv
 import config from './config.js';
 
 
@@ -12,6 +13,38 @@ app.get("/", (req, res) => {
     res.send(`Hello from ${name} running on ${os.hostname}`);
 });
 
+app.get("/nick-name/", async (req, res) => {
+    const adjectiveService = services.adjective;
+    const colorService = services.color;
+    const animalService = services.animal;
+
+    try{
+        const [adjRes, colorRes, animalRes] = await Promise.all([
+            fetch(`http://${adjectiveService}`),
+            fetch(`http://${colorService}`),
+            fetch(`http://${animalService}`),
+        ]);
+        const [adjective, color, animal] = await Promise.all([
+            adjRes.json(), 
+            colorRes.json(), 
+            animalRes.json()
+        ]);
+        res.status(200).send({
+            adjective,
+            color,
+            animal
+        });
+    }
+    catch(ex) {
+        console.error(ex);
+        res.status(500).send({
+            error: "Internal server error",
+            ex: ex,
+        });
+    }
+});
+
+// TODO: REMOVE THIS 
 app.get("/service/:name", async (req, res) => {
     const service = req.params.name;
     // NOTE: THIS URL WILL ONLY WORK WITH SERVICE DISCOVERY
