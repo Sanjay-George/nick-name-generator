b5184ed3bd517d5bd68c3566dc43de43e464e0d6
Sanjay-George <sanjaygeorge16@gmail.com>
2022-08-03 12:20:08 +0200

    Minor changes to config
    - Commented changes for local development
    
    Signed-off-by: Sanjay-George <sanjaygeorge16@gmail.com>

--- config.js
+++ config.js
@@ -1,6 +1,13 @@
 export default {
-    name: "nick-name-generator",
+    service: "nick-name-generator",
     port: 3000,
+    // LOCAL TESTING
+    // services: {
+    //     adjective: "127.0.0.1:53100",
+    //     color: "127.0.0.1:53102",
+    //     animal: "animal-service",
+    // },
+    // KUBE 
     services: {
         adjective: "adjective-service",
         color: "color-service",
